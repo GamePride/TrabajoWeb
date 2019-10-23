508b6593f7e9de07e5d36c1d60e2d5598c8ded47
Cezzard99 <u201615014@upc.edu.pe>
2019-10-23 14:57:23 -0500

    first commit boot

---
+++ TrabajoWeb/src/main/java/com/gamepride/platform/TrabajoWebApplication.java
@@ -0,0 +1,13 @@
+package com.gamepride.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoWebApplication.class, args);
+	}
+
+}
