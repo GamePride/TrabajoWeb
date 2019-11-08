c9b629620ac967251b78d20cebff15f3cdb252e3
Cezzard99 <u201615014@upc.edu.pe>
2019-11-08 12:35:26 -0500

    repository person

--- TrabajoWeb/src/main/java/com/gamepride/platform/repository/IPersonRepository.java
+++ TrabajoWeb/src/main/java/com/gamepride/platform/repository/IPersonRepository.java
@@ -1,8 +1,10 @@
 package com.gamepride.platform.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gamepride.platform.model.Person;
@@ -11,4 +13,7 @@ import com.gamepride.platform.model.Person;
 public interface IPersonRepository extends JpaRepository<Person, Long> {
 	
 	List<Person> findByType(String type);
+	
+	@Query("select p from Person p left join fetch p.lancenters l where p.id=?1")
+	Optional<Person> fetchByPersonIdWithLanCenters(Long id);
 }
