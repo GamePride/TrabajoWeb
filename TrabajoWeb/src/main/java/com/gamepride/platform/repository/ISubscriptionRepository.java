b74fc97152b2f621a5fb87c15689e4e4bfb6e2e5
carlos07071 <u201617072@upc.edu.pe>
2019-11-08 12:22:11 -0500

    Last correction

--- TrabajoWeb/src/main/java/com/gamepride/platform/repository/ISubscriptionRepository.java
+++ TrabajoWeb/src/main/java/com/gamepride/platform/repository/ISubscriptionRepository.java
@@ -1,16 +1,20 @@
 package com.gamepride.platform.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.gamepride.platform.model.Person;
 import com.gamepride.platform.model.Subscription;
 
 @Repository
 public interface ISubscriptionRepository extends JpaRepository<Subscription, Long>{
 
-	@Query("SELECT e FROM Subscription e WHERE e.name LIKE %?1%")
-	List<Subscription> findSubscriptionByName(String name);
+	
+	List<Subscription> findByName(String name);
+	@Query("SELECT e FROM Subscription s left join fetch s.gamers WHERE s.name LIKE %?1%")
+	Optional<Subscription> fetchBySubscriptionIdWithGamers(Long id);
 }
