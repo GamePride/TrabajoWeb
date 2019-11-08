b335ff3f67654fc212d606112b05fe3cd92797e3
Cezzard99 <u201615014@upc.edu.pe>
2019-11-08 12:17:03 -0500

    gamer fix conflict

--- TrabajoWeb/src/main/java/com/gamepride/platform/model/Gamer.java
+++ TrabajoWeb/src/main/java/com/gamepride/platform/model/Gamer.java
@@ -13,11 +13,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name="gamers")
