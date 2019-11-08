6133ca34972fd44538c1d108ee7d1fbb96ff216d
carlos07071 <u201617072@upc.edu.pe>
2019-11-08 14:11:42 -0500

    Fixes

--- TrabajoWeb/src/main/java/com/gamepride/platform/repository/IEventRepository.java
+++ TrabajoWeb/src/main/java/com/gamepride/platform/repository/IEventRepository.java
@@ -1,5 +1,7 @@
 package com.gamepride.platform.repository;
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -13,12 +15,15 @@ public interface IEventRepository extends JpaRepository<Event, Long> {
 	
 	@Modifying
 	@Query(value = "UPDATE events set published=true where id =?1 and vacancy>=10", nativeQuery = true)
-	void publishEvent(Integer id);
+	void publishEvent(Long id);
 	
 	@Modifying
 	@Query(value = "UPDATE events set published=false where id =?1", nativeQuery = true)
-	void saveEvent(Integer id);
+	void disableEvent(Long id);
 	
 	@Query("SELECT e FROM Event e WHERE e.name LIKE %?1%")
-	List<Event> findEventByName(String name);
+	List<Event> fetchEventByName(String name);
+	
+	@Query("select e from Event e left join fetch e.gamers g where e.id=?1")
+	Optional<Event> fetchByEventIdWithGamers(Long id);
 }
--- TrabajoWeb/src/main/java/com/gamepride/platform/repository/IGamerRepository.java
+++ TrabajoWeb/src/main/java/com/gamepride/platform/repository/IGamerRepository.java
@@ -1,6 +1,7 @@
 package com.gamepride.platform.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +12,8 @@ import com.gamepride.platform.model.Gamer;
 @Repository
 public interface IGamerRepository extends JpaRepository<Gamer, Long> {
 	
-	@Query("SELECT e FROM Gamer e WHERE e.name LIKE %?1%")
-	List<Gamer> findGamerByName(String name);
+	List<Gamer> findByUsername(String username);
+	
+	@Query("select g from Gamer g left join fetch g.events e where g.id=?1")
+	Optional<Gamer> fetchByGamerIdWithEvents(Long id);
 }
