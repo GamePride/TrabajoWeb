62c6359ddc93353b920b5bdd106d3afc0f64b85d
carlos07071 <u201617072@upc.edu.pe>
2019-11-08 18:47:11 -0500

    Person and Plan

--- TrabajoWeb/src/main/java/com/gamepride/platform/service/PlanService.java
+++ TrabajoWeb/src/main/java/com/gamepride/platform/service/PlanService.java
@@ -1,6 +1,5 @@
 package com.gamepride.platform.service;
 
-<<<<<<< HEAD
 import java.util.List;
 
 import com.gamepride.platform.model.Plan;
@@ -8,8 +7,5 @@ import com.gamepride.platform.model.Plan;
 public interface PlanService extends CrudService<Plan> {
 
 	List<Plan> fetchPlanById(Long id) throws Exception;
-=======
-public interface PlanService {
 
->>>>>>> aa44e1ce71507c0ed73ca00ce9919d4a41ad3457
 }
