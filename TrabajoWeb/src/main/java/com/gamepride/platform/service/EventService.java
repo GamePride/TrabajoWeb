690f23ecc68ae19763d19fb60f4cc9d737f02820
AdrianChuchullo <u201612189@upc.edu.pe>
2019-11-08 15:55:33 -0500

    service: event,gamer

--- TrabajoWeb/src/main/java/com/gamepride/platform/service/EventService.java
+++ TrabajoWeb/src/main/java/com/gamepride/platform/service/EventService.java
@@ -1,7 +1,12 @@
 package com.gamepride.platform.service;
 
+import java.util.List;
+
 import com.gamepride.platform.model.Event;
 
 public interface EventService extends CrudService<Event>{
 
+	
+	List<Event> fetchEventByName(String name);
+	
 }
