597087e2f248a433aa87d5c9b142c9154bb6837f
AdrianChuchullo <u201612189@upc.edu.pe>
2019-11-08 19:46:49 -0500

    crud service creation

--- TrabajoWeb/src/main/java/com/gamepride/platform/service/CrudService.java
+++ TrabajoWeb/src/main/java/com/gamepride/platform/service/CrudService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CrudService<T> {
+	
 	public List<T> findAll() throws Exception;
+	
 	public T save(T t) throws Exception;
-	public Optional <T> findById (Long id) throws Exception;
-	public void deleteById(Long id)throws Exception;
 	
+	public Optional<T> findById(Long id) throws Exception;
 	
+	public void deleteById(Long id) throws Exception;
 }
