6b9bcb0bc80374450b0199de4160713bb406993e
Cezzard99 <u201615014@upc.edu.pe>
2019-11-08 19:26:14 -0500

    lancenter repository

--- TrabajoWeb/src/main/java/com/gamepride/platform/repository/ILanCenterRepository.java
+++ TrabajoWeb/src/main/java/com/gamepride/platform/repository/ILanCenterRepository.java
@@ -3,6 +3,7 @@ package com.gamepride.platform.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gamepride.platform.model.LanCenter;
@@ -12,5 +13,6 @@ public interface ILanCenterRepository extends JpaRepository<LanCenter, Long> {
 	
 	List<LanCenter> findByName(String name);
 	
-	
+	@Query("select l from LanCenter l join fetch l.personId e join fetch e.gamers g where l.id=?1")
+	List<LanCenter> fetchByLanCenterIdWithEventsWithGamers(Long id);	
 }
